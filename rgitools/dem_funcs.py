465ada7a028c802e377e5d1abb268b95e4fa9549
Matthias Dusch <matthias.dusch@uibk.ac.at>
2019-12-20 15:51:32 +0100

    xmas commit

--- rgitools/dem_funcs.py
+++ rgitools/dem_funcs.py
@@ -1,12 +1,9 @@
-import os
 import logging
-import tarfile
 
-import pandas as pd
 import numpy as np
 import rasterio
 
-from oggm import utils, GlacierDirectory, entity_task
+from oggm import GlacierDirectory, entity_task
 
 # Module logger
 log = logging.getLogger(__name__)
@@ -18,12 +15,24 @@ def dem_quality(gdir, demfile):
     Parameters
     ----------
     gdir : :py:class:`oggm.GlacierDirectory`
-        where to write the data
+        the glacier in question
+    demfile : str
+        path to a specific DEM tif-file
 
     Returns
     -------
     nanpercent : float
         how many grid points are NaN as a fraction of all grid points
+    nanpercent_glc : float
+        how many grid points are NaN as a fraction of all glaciated grid points
+    meanhgt : float
+        mean elevation of grid points
+    meanhgt_glc : float
+        mean elevation of glaciated grid points
+    roughness : float
+        standard deviation of 2d slope of all grid points
+    roughness_glc : float
+        standard deviation of 2d slope of all glaciated grid points
     """
 
     # open tif-file:
@@ -31,106 +40,68 @@ def dem_quality(gdir, demfile):
         dem = ds.read(1).astype(rasterio.float32)
         nx = ds.width
         ny = ds.height
-
-    # Grid
+        dx = ds.transform[0]
+    # assert some basics
     assert nx == gdir.grid.nx
     assert ny == gdir.grid.ny
+    assert dx == gdir.grid.dx
 
+    # open glacier mask
+    with rasterio.open(gdir.get_filepath('glacier_mask'),
+                       'r', driver='GTiff') as ds:
+        mask = ds.read(1).astype(rasterio.int16)
+
+    # set nodata values to NaN
     min_z = -999.
     dem[dem <= min_z] = np.NaN
     isfinite = np.isfinite(dem)
+    isfinite_glc = np.isfinite(dem[np.where(mask)])
 
+    # calculate fraction of NaNs in all and glaciated area
     nanpercent = np.sum(isfinite) / (nx * ny)
+    nanpercent_glc = np.sum(isfinite_glc) / mask.sum()
 
+    # calculate mean elevation of all and glaciated area
     meanhgt = np.nanmean(dem)
+    meanhgt_glc = np.nanmean(dem[np.where(mask)])
 
-    # TODO : use some proper roughness measure. For now just std
-    rough = np.nanstd(dem)
+    # calculate roughness of all area
+    sy, sx = np.gradient(dem, dx)
+    slope = np.arctan(np.sqrt(sy**2 + sx**2))
+    roughness = np.nanstd(slope)
 
-    return nanpercent, meanhgt, rough
+    # calculate roughness of glaciated area
+    dem_glc = np.where(mask, dem, np.nan)
+    sy, sx = np.gradient(dem_glc, dx)
+    slope = np.arctan(np.sqrt(sy**2 + sx**2))
+    roughness_glc = np.nanstd(slope)
 
+    return (nanpercent, nanpercent_glc, meanhgt, meanhgt_glc, roughness,
+            roughness_glc)
 
-def gdirs_from_tar_files(path, rgi_region=None):
-
-    gdirs = []
-    for regdir in os.listdir(path):
-
-        # only do required rgi_region
-        if (rgi_region is not None) and (regdir[-2:] != rgi_region):
-            continue
-
-        rdpath = os.path.join(path, regdir)
-
-        for file in os.listdir(rdpath):
-
-            with tarfile.open(os.path.join(rdpath, file), 'r') as tfile:
-                for member in tfile:
-                    if member.isdir():
-                        continue
-                    tar_base = os.path.join(rdpath, member.path)
-                    gdirs.append(GlacierDirectory(member.name[-21:-7],
-                                                  from_tar=tar_base))
-
-    return gdirs
-
-
-@entity_task(log)
-def check_gdir_dems(gdir):
-    """
-
-    :param gdir:
-    :return:
-    """
-
-    # dataframe for results
-    df = pd.DataFrame([], index=[gdir.rgi_id]*3, # np.arange(3),
-                      columns=['metric'] + utils.DEM_SOURCES)
-    df.iloc[0]['metric'] = 'quality'
-    df.iloc[1]['metric'] = 'meanhgt'
-    df.iloc[2]['metric'] = 'roughness'
-
-
-    logfile = (os.path.join(gdir.dir, 'log.txt'))
-
-    # read logfile
-    lfdf = pd.read_csv(logfile, delimiter=';', header=None,
-                       skipinitialspace=True)
-
-    # loop over dems and save existing ones to test
-    dem2test = []
-    for _, line in lfdf.iterrows():
-        if ('DEM SOURCE' in line[1]) and ('SUCCESS' in line[2]):
-            rgi = line[1].split(',')[0]
-            dem = line[1].split(',')[2]
-            dem2test.append(dem)
-
-    # loop over DEMs
-    for dem in dem2test:
-        demfile = os.path.join(gdir.dir, dem) + '/dem.tif'
-        nans, mhgt, rough = dem_quality(gdir, demfile)
-        df.loc[df.metric == 'quality', dem] = nans
-        df.loc[df.metric == 'meanhgt', dem] = mhgt
-        df.loc[df.metric == 'roughness', dem] = rough
-
-    return df
 
 @entity_task(log)
 def get_dem_area(gdir):
-    """
+    """Read the glacier_mask.tif and calculated glacier area based on this
 
-    :param gdir:
-    :return:
+    Parameters
+    ----------
+    gdir : GlacierDirectory
+        the glacier in question
+
+    Returns
+    -------
+    float
+        glacier area in km2
     """
 
     # read dem mask
-    with rasterio.open(gdir.get_filepath('dem_mask'),
+    with rasterio.open(gdir.get_filepath('glacier_mask'),
                        'r', driver='GTiff') as ds:
         profile = ds.profile
         data = ds.read(1).astype(profile['dtype'])
 
     # calculate dem_mask size and test against RGI area
     mask_area_km2 = data.sum() * gdir.grid.dx**2 * 1e-6
-
-    #np.testing.assert_almost_equal(mask_area_km2,gdir.rgi_area_km2, decimal=1)
 
     return mask_area_km2
